e215de8c8f8669aa9b3224c7edb5ea807bc0329b
bencicco <your.email@example.com>
2025-09-09 11:58:28 +1000

    conn.commit()

--- api/utils/database/business_register.py
+++ api/utils/database/business_register.py
@@ -1,32 +1,14 @@
 from typing import List
 import pyodbc
 
-def insert_business(server_creds, email, name, opted_out, contacting):
-    conn = pyodbc.connect(
-    f'DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    )
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO Business_Registrar (
-            Contact_Email,
-            Business_Name,
-            Opted_Out,
-            Currently_Contacting
-        )
-        VALUES (?, ?, ?, ?);
-    """, (
-        email,
-        name,
-        opted_out,   # Opted_Out
-        contacting    # Currently_Contacting
-    ))
 
 def insert_business(server_creds, email, name, opted_out, contacting):
     conn = pyodbc.connect(
-    f'DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+        f"DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
     )
     cursor = conn.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT INTO Business_Registrar (
             Contact_Email,
             Business_Name,
@@ -34,22 +16,44 @@ def insert_business(server_creds, email, name, opted_out, contacting):
             Currently_Contacting
         )
         VALUES (?, ?, ?, ?);
-    """, (
-        email,
-        name,
-        opted_out,   # Opted_Out
-        contacting    # Currently_Contacting
-    ))
+    """,
+        (email, name, opted_out, contacting),  # Opted_Out  # Currently_Contacting
+    )
+
+
+def insert_business(server_creds, email, name, opted_out, contacting):
+    conn = pyodbc.connect(
+        f"DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    )
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO Business_Registrar (
+            Contact_Email,
+            Business_Name,
+            Opted_Out,
+            Currently_Contacting
+        )
+        VALUES (?, ?, ?, ?);
+    """,
+        (email, name, opted_out, contacting),  # Opted_Out  # Currently_Contacting
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
 
 def opt_out_business(server_creds, email):
     conn = pyodbc.connect(
-    f'DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+        f"DRIVER={server_creds[0]};SERVER={server_creds[1]};DATABASE={server_creds[2]};UID={server_creds[3]};PWD={server_creds[4]};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
     )
     cursor = conn.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
     UPDATE Business_Registrar
     SET Opted_Out = 1
     WHERE Contact_Email = ?;
-""", (email,))
-
-   
+""",
+        (email),
+    )
