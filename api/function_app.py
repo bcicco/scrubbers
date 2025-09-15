2407b145225d1392c0093b8936004c248ace65cb
bencicco <your.email@example.com>
2025-09-15 17:18:31 +1000

    first commiy for percy prompt palace

--- api/function_app.py
+++ api/function_app.py
@@ -5,12 +5,12 @@ from blueprints.database_blueprint import databaseBP
 from blueprints.mail_blueprint import mailBP
 from blueprints.readmailBP import readmailBP
 """Uncomment below for Local Dev"""
-#load_dotenv()
+load_dotenv()
 
 # Instantiate App
 app = func.FunctionApp()
 app.register_functions(openAIBP)
 app.register_functions(databaseBP)
-app.register_functions(mailBP)
-app.register_functions(readmailBP)
+#app.register_functions(mailBP)
+#app.register_functions(readmailBP)
 
