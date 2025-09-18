2bdffa23f23ea85a4010589c7f77891c70b181cf
bencicco <your.email@example.com>
2025-09-18 15:25:19 +1000

    added some other shit

--- api/function_app.py
+++ api/function_app.py
@@ -2,15 +2,13 @@ import azure.functions as func
 from dotenv import load_dotenv
 from blueprints.openAI_blueprint import openAIBP
 from blueprints.database_blueprint import databaseBP
-from blueprints.mail_blueprint import mailBP
 from blueprints.readmailBP import readmailBP
 """Uncomment below for Local Dev"""
-load_dotenv()
+#load_dotenv()
 
 # Instantiate App
 app = func.FunctionApp()
 app.register_functions(openAIBP)
 app.register_functions(databaseBP)
-#app.register_functions(mailBP)
-#app.register_functions(readmailBP)
+app.register_functions(readmailBP)
 
